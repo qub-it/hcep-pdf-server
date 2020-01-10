11c2b0497db004880c61a1005ddb5357731e9f40
Daniel Pires <daniel.pires@qub-it.com>
2020-01-10 15:45:26 +0000

    removed unnecessary options

--- app/express-app.js
+++ app/express-app.js
@@ -106,68 +106,15 @@ module.exports.expressApp = pages => {
 
         try {
           
-          
-          var fonts = ""
-          if(req.body.fonts){
-            fonts = req.body.fonts
-          }
-          
-          await page.setContent(fonts + html)
-          debug(`html received:${html}`)
-          
-          // Wait for web font loading completion
-          // await page.evaluateHandle('document.fonts.ready')
           const pdfOption = getPdfOption(req.body.pdf_option)
           debug(`using PDFOption:${pdfOption}`)
           
-
-          const header = req.body.header
-          if(header){
-            debug(`header received:${header}`)
-            // webkit-print-color-adjust must be set to exact
-            // to set background colors, due to issue in we-kit
-            //
-            // For more information see - https://github.com/puppeteer/puppeteer/issues/2182
-            //
-            //  
-            // Header and Footer doesn't support certain custom css
-            //
-            // Fore more information see - https://github.com/puppeteer/puppeteer/issues/1853
-            //                             https://github.com/puppeteer/puppeteer/issues/2388
-            //                             https://github.com/puppeteer/puppeteer/issues/2916
-            //
-            // Daniel Pires - 08 January 2020
-            //
-            pdfOption.headerTemplate = "<style> * {-webkit-print-color-adjust: exact}</style> " + header
-          }
-
-          const footer = req.body.footer
-          if(footer){
-            debug(`footer received:${footer}`)
-            // webkit-print-color-adjust must be set to exact
-            // to set background colors, due to issue in we-kit
-            //
-            // For more information see - https://github.com/puppeteer/puppeteer/issues/2182
-            //
-            //  
-            // Header and Footer doesn't support certain custom css
-            //
-            // Fore more information see - https://github.com/puppeteer/puppeteer/issues/1853
-            //                             https://github.com/puppeteer/puppeteer/issues/2388
-            //                             https://github.com/puppeteer/puppeteer/issues/2916
-            //
-            // Daniel Pires - 08 January 2020
-            //
-            pdfOption.footerTemplate = "<style> * {-webkit-print-color-adjust: exact}</style> " + footer
-          }
-
-          if(header || footer) {
-            pdfOption.displayHeaderFooter = true;
-          } else {
-            pdfOption.displayHeaderFooter = false;
-          }
-
+                    
+          await page.setContent(html)
+          debug(`html received:${html}`)
           
+          pdfOption.displayHeaderFooter = false;
+
           const leftMargin = req.body.leftMargin;
           if(leftMargin) {
             debug(`setting Margin-Left:${leftMargin}`)
@@ -178,50 +125,6 @@ module.exports.expressApp = pages => {
           if(leftMargin) {
             debug(`setting Margin-Right:${rightMargin}`)
             pdfOption.margin.right = rightMargin
-          }
-          
-          const headerHeight = req.body.headerHeight;
-          if(headerHeight) {
-            debug(`setting Margin-Top:${headerHeight}`)
-            if(headerHeight != "0px"){
-              // This has to be fixed, it has several issues
-              // due to the way puppeteer generates header and footer
-              // separately from the body
-              // 
-              // - Different DPIs, makes sizes different
-              // - Different resolutions from the editor view
-              // 
-              // We added the left and right margins to the footer
-              // height so it has more space
-              //
-              // Daniel Pires - 08 January 2020
-              var oldHeight = parseInt(headerHeight.replace("px",""))
-              var newHeight = oldHeight + (72 * parseInt(pdfOption.margin.left.replace("mm",""))/25.4) + (72 * parseInt(pdfOption.margin.right.replace("mm",""))/25.4)
-              pdfOption.margin.top = String(parseInt(newHeight)) + "px";
-            }
-          }
-
-          const footerHeight = req.body.footerHeight;
-          if(footerHeight) {
-            debug(`setting Margin-Bottom:${footerHeight}`)
-            if(footerHeight != "0px"){
-
-              // This has to be fixed, it has several issues
-              // due to the way puppeteer generates header and footer
-              // separately from the body
-              // 
-              // - Different DPIs, makes sizes different
-              // - Different resolutions from the editor view
-              // 
-              // We added the left and right margins to the footer
-              // height so it has more space
-              //
-              // Daniel Pires - 08 January 2020
-              var oldHeight = parseInt(footerHeight.replace("px",""))
-              var newHeight = oldHeight + (72 * parseInt(pdfOption.margin.left.replace("mm",""))/25.4) + (72 * parseInt(pdfOption.margin.right.replace("mm",""))/25.4)
-
-              pdfOption.margin.bottom = String(parseInt(newHeight)) + "px";
-            }
           }
 
           // debug('pdfOption', pdfOption)
